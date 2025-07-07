280b7db23e74656f6f777656b3acaea5f4bd37bf
rdpajaron <romielle.pajaron@gcash.com>
2025-07-07 15:52:32 +0800

    "added checkProduct function"

--- src/main/java/org/example/GroceryInventorySystem.java
+++ src/main/java/org/example/GroceryInventorySystem.java
@@ -13,13 +13,14 @@ public class GroceryInventorySystem {
         do{
             GroceryInventorySystem groceryInventorySystem = new GroceryInventorySystem();
             System.out.println(
-                    "--- Grocery Inventory Menu\n" +
-                            "1. View Inventory\n" +
-                            "2. Add Product\n" +
-                            "3. Check Product\n" +
-                            "4. Update Stock\n" +
-                            "5. Remove product\n" +
-                            "6. Exit"
+                    """
+                            --- Grocery Inventory Menu
+                            1. View Inventory
+                            2. Add Product
+                            3. Check Product
+                            4. Update Stock
+                            5. Remove product
+                            6. Exit"""
             );
             System.out.print("Choose an option: ");
             option = Integer.parseInt(scanner.nextLine());
@@ -27,17 +28,30 @@ public class GroceryInventorySystem {
             switch(option){
                 case 1:
                     groceryInventorySystem.viewInventory();
+                    break;
                 case 2://add product
-                    System.out.print("Enter product name:");
+                    System.out.print("Enter product name: ");
                     String product = scanner.nextLine();
                     System.out.print("Enter quantity: ");
                     int quantity = Integer.parseInt(scanner.nextLine());
                     groceryInventorySystem.addProduct(product, quantity);
-                case 3://update stock
-
-                case 4:
+                    break;
+                case 3://check product
+                    System.out.print("Enter product name to check: ");
+                    product = scanner.nextLine();
+                    groceryInventorySystem.checkProduct(product);
+                    break;
+                case 4://update stock
+                    System.out.print("Enter product name to check: ");
+                    product = scanner.nextLine();
+                    System.out.println("Enter new stock quantity: ");
+                    int newQuantity = Integer.parseInt(scanner.nextLine());
+                    groceryInventorySystem.updateStock(product, newQuantity);
+                    break;
                 case 5:
+                    break;
                 case 6:
+                    break;
                 default:
                     System.out.println("Invalid option! Please try again.");
                     break;
@@ -49,16 +63,31 @@ public class GroceryInventorySystem {
     }
 
     public void viewInventory(){
-        productStock.forEach((k, v) -> {
-            System.out.println(k + " - " + v + "pcs");
-        });
+        productStock.forEach((k, v) -> System.out.println(k + " - " + v + "pcs"));
     }
 
     public void addProduct(String product, int quantity){
         if(productStock.containsKey(product)){
             System.out.println("Product already exists in inventory!");
+            return;
         }
         productStock.put(product, quantity);
+    }
+
+    public void updateStock(String product, int newQuantity){
+        if(!productStock.containsKey(product)){
+            System.out.println("Product does not exist!");
+            return;
+        }
+        productStock.replace(product, newQuantity);
+    }
+
+    public void checkProduct(String product){
+        if(!productStock.containsKey(product)){
+            System.out.println("Product does not exist!");
+            return;
+        }
+        System.out.println(product + " is in stock: " + productStock.get(product));
     }
 
 }
